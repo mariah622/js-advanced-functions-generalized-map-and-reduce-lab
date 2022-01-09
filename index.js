357fad606abdfe092d2b01b39e0b07c2d0654b8f
mariah622 <mjustine622@gmail.com>
2022-01-09 11:11:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions here
+function map(integer, callback){
+    let a = []
+
+    for(let i = 0; i < integer.length; i++){
+        const thing = integer[i]
+        a.push(callback(thing))
+    }
+    return a 
+
+    
+}
+
+
+function reduce(integer, callback, startingPoint){
+    let total
+
+    if(startingPoint){
+        total = startingPoint;
+
+        for(let i = 0; i < integer.length; i++){
+            total = callback(integer[i], total);
+        }
+
+        return total;
+
+    } else {
+        total = integer[0]
+        for(let i = 1; i < integer.length; i++){
+            total = callback(integer[i], total);
+        }
+        return total;
+    }
+
+
+
+}
